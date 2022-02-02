ad033f1c8390de55632a2f820b7f09c4de3dabbb
Jiani Cao <jianicakes@gmail.com>
2022-02-01 16:46:30 -0800

    character counter now turns red when over limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,19 @@
-console.log('Hello');
+$(document).ready(function() {
+
+  $('textarea').on('keyup', function() {
+    const tweetLength = $(this).val().length;
+    const charRemaining = 140 - tweetLength;
+
+    //<textarea> and <div> which contains counter element are both children of <form>
+    //access character counter through siblings
+    const tweetCounter = $(this).siblings().children('.counter');
+    tweetCounter.val(charRemaining);
+
+    //color counter numbers red if below zero
+    if (charRemaining < 0) {
+      return $(tweetCounter).css("color", "#eb6134");
+    }
+    $(tweetCounter).css("color", "#545149");
+
+  });
+});
