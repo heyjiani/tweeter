fc2ffad5b5818aa672f68a4e811184f559f7c822
Jiani Cao <jianicakes@gmail.com>
2022-02-02 15:36:11 -0800

    implement renderTweets function for appending tweet objects to tweets-container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,19 +1,29 @@
-console.log('hello hello')
-
 $(() => {
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
     },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 //takes in tweet obj and returns tweet <article> element
 const createTweetElement = function (tweet) {
@@ -42,16 +52,25 @@ const createTweetElement = function (tweet) {
   $footer.append($timeCreated, $icons);
 
   //full tweet 
-  const $fullTweet = $('<article>').addClass('tweet');
-  $fullTweet.append($header, $tweetText, $footer);
+  const $tweet = $('<article>').addClass('tweet');
+  $tweet.append($header, $tweetText, $footer);
 
-  return $fullTweet;
+  return $tweet;
 };
 
-const $tweet = createTweetElement(tweetData);
+//take in array of tweet objs and append each one to .tweet-container
+const $tweetContainer = $('.tweet-container');
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('.tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+const renderTweets = function(listOfTweets) {
+
+  for (const tweet of listOfTweets) {
+    const $tweet = createTweetElement(tweet);
+    $tweetContainer.append($tweet);
+  }
+  return $tweetContainer;
+
+};
+
+renderTweets(data);
 
 });
